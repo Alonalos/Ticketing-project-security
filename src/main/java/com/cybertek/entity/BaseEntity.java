cf4f7afe43e8d39712ed49c9aa98f7ac7fe7136a
Alonalos <alonalos8@gmail.com>
2021-01-05 20:10:51 -0500

    ORM

---
+++ src/main/java/com/cybertek/entity/BaseEntity.java
@@ -0,0 +1,41 @@
+package com.cybertek.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime insertDateTime;
+    private Long insertUserId;
+    private LocalDateTime lastUpdateDateTime;
+    private Long lastUpdateUserId;
+
+    private Boolean isDeleted=false;
+
+    @PrePersist //before saving to the db
+    private void onPrePersist(){
+        this.insertDateTime=LocalDateTime.now();
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.insertUserId=1L;
+        this.lastUpdateUserId=1L;
+    }
+
+    @PreUpdate //before updating in the db
+    private void onPreUpdate(){
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.lastUpdateUserId=1L;
+    }
+
+}
